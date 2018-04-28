7f57fa9f2065410300f91f70a0131faf46e6ec88
Faulo <FauloLio@gmail.com>
2018-04-28 11:24:50 +0200

    Executables

---
+++ src/Slothsoft/Savegame/Executables/EditorExecutable.php
@@ -0,0 +1,23 @@
+<?php
+namespace Slothsoft\Savegame\Executables;
+
+use Slothsoft\Farah\Module\Executables\ExecutableBase;
+use Slothsoft\Farah\Module\FarahUrl\FarahUrlStreamIdentifier;
+use Slothsoft\Farah\Module\Results\ResultCreator;
+use Slothsoft\Savegame\Editor;
+use Slothsoft\Farah\Module\Results\ResultInterface;
+
+class EditorExecutable extends ExecutableBase
+{
+    private $editor;
+    public function __construct(Editor $editor) {
+        $this->editor = $editor;
+    }
+
+    protected function loadResult(FarahUrlStreamIdentifier $type) : ResultInterface
+    {
+        $creator = new ResultCreator($this, $type);
+        return $creator->createDOMWriterResult($this->editor);
+    }
+}
+
---
+++ src/Slothsoft/Savegame/Executables/SavegameExecutableCreator.php
@@ -0,0 +1,13 @@
+<?php
+namespace Slothsoft\Savegame\Executables;
+
+use Slothsoft\Farah\Module\Executables\ExecutableCreator;
+use Slothsoft\Farah\Module\Executables\ExecutableInterface;
+use Slothsoft\Savegame\Editor;
+
+class SavegameExecutableCreator extends ExecutableCreator {
+    public function createEditorExecutable(Editor $editor) : ExecutableInterface {
+        return $this->initExecutable(new EditorExecutable($editor));
+    }
+}
+
