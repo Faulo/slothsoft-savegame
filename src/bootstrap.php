eb6d12598c600fade0755dbf81795a9ece47f084
Daniel Schulz <faulolio@gmail.com>
2025-09-02 14:52:24 +0200

    devops: workflow

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,5 +1,6 @@
 <?php
 declare(strict_types = 1);
+namespace Slothsoft\Savegame;
 
 use Slothsoft\Farah\Module\Module;
 Module::registerWithXmlManifestAndDefaultAssets('slothsoft@savegame', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'assets');
