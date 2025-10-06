c5f65a3536a6ccea4842dba814a3f9607571ce72
Daniel Schulz <faulolio@gmail.com>
2025-10-06 15:19:10 +0200

    fix: ensureDirectory

--- src/Node/ArchiveParser/CopyArchiveExtractor.php
+++ src/Node/ArchiveParser/CopyArchiveExtractor.php
@@ -3,13 +3,12 @@ declare(strict_types = 1);
 namespace Slothsoft\Savegame\Node\ArchiveParser;
 
 use SplFileInfo;
+use Slothsoft\Core\FileSystem;
 
 class CopyArchiveExtractor implements ArchiveExtractorInterface {
     
     public function extractArchive(SplFileInfo $archivePath, SplFileInfo $targetDirectory): bool {
-        if (! $targetDirectory->isDir()) {
-            mkdir((string) $targetDirectory, 0777, true);
-        }
+        FileSystem::ensureDirectory((string) $targetDirectory);
         return copy((string) $archivePath, $targetDirectory . DIRECTORY_SEPARATOR . '1');
     }
 }
