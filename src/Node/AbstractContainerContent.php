63eb4fa167a0b4a88da840f209fdb3c8cbd3d454
Daniel Schulz (Madeline) <faulolio@gmail.com>
2025-12-26 20:40:43 +0100

    fix(container): getBuildAttributes

--- src/Node/AbstractContainerContent.php
+++ src/Node/AbstractContainerContent.php
@@ -4,8 +4,15 @@ namespace Slothsoft\Savegame\Node;
 
 use Slothsoft\Core\XML\LeanElement;
 use Slothsoft\Savegame\Build\BuildableInterface;
+use Slothsoft\Savegame\Build\BuilderInterface;
 
 abstract class AbstractContainerContent extends AbstractContentNode implements BuildableInterface {
     
     protected function loadContent(LeanElement $strucElement): void {}
+    
+    public function getBuildAttributes(BuilderInterface $builder): array {
+        return [
+            'name' => $this->name
+        ];
+    }
 }
